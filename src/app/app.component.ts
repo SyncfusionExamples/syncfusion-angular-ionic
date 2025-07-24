7164a07796aa6a39eb5e29b0e10a196ea6f0006e
Manikandan-Lakshmanan <manikandan.lakshmanan@syncfusion.com>
2025-07-24 10:48:53 +0530

    970876: Need to Update the sample to latest structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  standalone: true,
+  imports: [IonicModule, RouterModule],
+  template: `
+    <ion-app>
+      <ion-router-outlet></ion-router-outlet>
+    </ion-app>
+  `
 })
-export class AppComponent {
-  constructor() {}
-}
+export class AppComponent {}
